5e4d0638a44b16ed10253b4d3b04b80fa4a13dc9
Martincz Gao <martincz@mokeedev.com>
2019-08-11 17:57:38 +0800

    m8d: mokee-ify

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,4 +14,4 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_m8d.mk
+    $(LOCAL_DIR)/mk_m8d.mk
--- mk_m8d.mk
+++ mk_m8d.mk
@@ -18,11 +18,11 @@
 # Inherit from m8d device
 $(call inherit-product, device/htc/m8d/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common MK stuff.
+$(call inherit-product, vendor/mk/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_m8d
+PRODUCT_NAME := mk_m8d
 PRODUCT_DEVICE := m8d
 PRODUCT_BRAND := htc
 PRODUCT_MODEL := m8d
